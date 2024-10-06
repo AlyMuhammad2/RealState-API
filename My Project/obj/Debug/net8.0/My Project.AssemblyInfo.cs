b64e3355c418e2c4736cdff6cf681d233ea12ed6
mohamedtarek01141 <mohamedtarek1303@gmail.com>
2024-10-07 02:41:05 +0300

    add payment

--- My Project/obj/Debug/net8.0/My Project.AssemblyInfo.cs
+++ My Project/obj/Debug/net8.0/My Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("My Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6297266e27e304e633795735eca2bbb33784018e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0d58f6ea92fc309d8898e0c4ad67c1376adf575")]
 [assembly: System.Reflection.AssemblyProductAttribute("My Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("My Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
